ff1f048f837fb933ab6a868ac511f3ba6f8a8868
Vladimir <falinvlad93@rambler.ru>
2022-11-19 18:41:25 +0300

    fix package

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:twoGophers/twoGophers/nuxt-best-techniq.git master:gh-pages
+git push -f git@github.com:twoGophers/nuxt-best-techniq.git master:gh-pages
 
 cd -
